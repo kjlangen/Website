b5520fedd6f7899bfd78b36fcaa086c664d16c56
kjlangen <fenwickhawks@gmail.com>
2014-06-09 22:48:23 -0400

    Added Javascript to center the number facts from Numbers Api, but having trouble's with a get request

--- numbers_api.js
+++ numbers_api.js
@@ -1,12 +1,29 @@
 var rand = Math.floor((Math.random() * 100) + 1);
 $(document).ready(function () {
+    "Use Strict";
 
     var html_height = $("html").height();
     var window_height = $(window).height();
     var difference = (window_height - html_height);
+
+    function get_num_fact(){
+      $.get("http://numbersapi.com/" + rand + "/math",
+            function (data){ $("#number_fact").text(data);
+                             window.console.log("Finished adding number_api text");
+      });
+    }
+
     $("footer").css("bottom", -1 * difference);
 
-    $.get("http://numbersapi.com/" + rand + "/math", function (data){
-      $("#number_fact").text(data);
-    });
+    get_num_fact();
+
+    var quote_height = $("#number_fact").height();
+    var hexagon_height = $(".right_hex_wrapper").height();
+
+    $("#number_fact").css("top", hexagon_height/3 - quote_height/2);
+    $("#credit").css("top", hexagon_height/3 - quote_height/2);
+
+    window.console.log(hexagon_height);
+    window.console.log(quote_height);
+    window.console.log(hexagon_height/3 - quote_height/2);
 });
