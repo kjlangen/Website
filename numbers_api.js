424908f9297309cb825ed371b7b5b60a882794d0
Ascension server <kjlangen@Ascension.(none)>
2014-07-02 09:43:09 -0400

    Added JS to animate main hexagon.

--- numbers_api.js
+++ numbers_api.js
@@ -10,9 +10,11 @@ $(document).ready(function () {
     var banner = $(".banner_wrapper");
     var icon = [$("#facebook"), $("#github"), $("#resume"), $("#linkedin")];
     var icon_img = [];
-    var inner_text = [$('.work_experience')];
-    var fill = [$('#exp_fill')];
+    var inner_text = [$(".work_experience"),$(".projects"),$(".blog")];
+    var inner_lines = [$(".inner_right"),$(".inner_left"),$(".inner_vertical")];
+    var fill = [$("#exp_fill")];
 
+    //Initialize icon_img array
     for(var i = 0; i < 4; i++){
       icon_img[i] = icon[i].find("img");
     }
@@ -55,6 +57,30 @@ $(document).ready(function () {
         function (){
           $(this).closest("div").find("p").fadeOut("slow");
         }
+      );
+    }
+
+    //On Hover Animate Center Hex
+    for(var i = 0; i < 1; i++){
+      var first = (i + 1) % 3;
+      var second = (i + 2) % 3;
+      inner_text[i].hover(
+	function (){
+	  inner_text[first].fadeOut("fast");
+	  inner_text[second].fadeOut("fast");
+	  inner_lines[second].fadeOut("fast");
+	  inner_lines[i].animate({top:"+=125"});
+	  inner_lines[first].animate({top:"+=125"});
+	  fill[i].fadeIn("slow");
+	},
+	function (){
+	  fill[i].fadeOut("slow");
+	  inner_lines[i].animate({top:"-=125"});
+	  inner_lines[first].animate({top:"-=125"});
+	  inner_lines[second].fadeIn("fast");
+	  inner_text[first].fadeIn("fast");
+	  inner_text[second].fadeIn("fast");
+	}
       );
     }
 
