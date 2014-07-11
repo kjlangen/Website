56d31a1628f2e9d8207595b43bedc88248920d14
kjlangen <fenwickhawks@gmail.com>
2014-07-11 12:26:10 -0400

    Minor change to account for fonts loading after JS is executed.

--- ProjectsPage/Projects.js
+++ ProjectsPage/Projects.js
@@ -5,13 +5,14 @@ $(document).ready(function () {
   var horiz;
 
   for(var k = 0; k < graphics.length; k++){
-    horiz = 2 * graphics[k].find(".leftbrace").width() + graphics[k].find(".project_title").width();
+    horiz = 2 * graphics[k].find(".leftbrace").width() + graphics[k].find(".project_title").width() + 15;
     graphics[k].width(horiz);
   }
 
   //Equally space the banner items
   var banner = $(".banner_wrapper");
   var icon = [$("#facebook"), $("#github"), $("#resume"), $("#linkedin"), $("#home")];
+
   var icon_img = [];
 
   for(var i = 0; i < 5; i++){
