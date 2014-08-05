c0c053719228dacadef01fcae7afd064cd14d4e1
kjlangen <fenwickhawks@gmail.com>
2014-08-04 22:44:28 -0400

    Worked on responsiveness for projects page.

--- ProjectsPage/Projects.js
+++ ProjectsPage/Projects.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
   "Use Strict";
 
   //Setup title graphics
-  var graphics = [$("#server_graphic"), $("#web_graphic"), $("#gol_graphic")];
+  /*var graphics = [$("#server_graphic"), $("#web_graphic"), $("#gol_graphic")];
   var horiz;
 
   for(var k = 0; k < graphics.length; k++){
     horiz = 2 * graphics[k].find(".leftbrace").width() + graphics[k].find(".project_title").width() + 15;
     graphics[k].width(horiz);
-  }
+  }*/
 
   //Equally space the banner items
   var icon = [$("#facebook"), $("#github"), $("#resume"), $("#linkedin"), $("#home")];
