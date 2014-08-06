1615570c3562a3c22479452a30d3421969d9aa87
kjlangen <fenwickhawks@gmail.com>
2014-08-05 22:52:15 -0400

    Worked more on Django tutorial.

--- DynamicWebsite/polls/admin.py
+++ DynamicWebsite/polls/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
 
 class PollAdmin(admin.ModelAdmin):
-	fieldsets = {
+	fieldsets = [
 		(None, {'fields' : ['question']}),
 		('Date information', {'fields' : ['pub_date']})
-	}
+	]
+	inlines = [ChoiceInline]
+	list_display = ("question", "pub_date","was_published_recently")
+	list_filter = ["pub_date"]
+	search_fields = ["question"]
 
 # Register your models here.
 admin.site.register(Poll, PollAdmin)
--- DynamicWebsite/polls/models.py
+++ DynamicWebsite/polls/models.py
@@ -9,10 +9,14 @@ class Poll(models.Model):
 		return self.question
 
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=15)
 
+	was_published_recently.admin_order_field = "pub_date"
+	was_published_recently.boolean = True
+	was_published_recently.short_description = "Published recently?"
 	question = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+
 
 class Choice(models.Model):
 
