9157cdaaa88f421e2e3dbed88a726fec0ddba1b0
kjlangen <fenwickhawks@gmail.com>
2014-08-06 22:35:10 -0400

    Finished most relevant parts of Django tutorial.

--- DynamicWebsite/polls/views.py
+++ DynamicWebsite/polls/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<p>Hi! Looks like it's working!</p>");
+	return HttpResponse("<p>Hi! Looks like it's working!</p>")
+
+def detail(request, poll_id):
+	return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+	return HttpResponse("You're voting at poll %s." % poll_id)
