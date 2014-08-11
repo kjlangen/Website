54d037e51af0128cd4f709d2bb87eba27ca1b503
unknown <fenwickhawks@gmail.com>
2014-08-10 19:38:54 -0700

    Worked through a little bit more of the django tutorial.

--- DynamicWebsite/polls/views.py
+++ DynamicWebsite/polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<p>Hi! Looks like it's working!</p>")
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	output = ', '.join([p.question for p in latest_poll_list]);
+	return HttpResponse(output)
 
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll %s." % poll_id)
