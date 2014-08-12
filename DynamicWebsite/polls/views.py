9ab3021d4a7abfb536ac310637633e5c85ca96ca
unknown <fenwickhawks@gmail.com>
2014-08-11 22:42:35 -0700

    Went through the django templating tutorial.

--- DynamicWebsite/polls/views.py
+++ DynamicWebsite/polls/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from polls.models import Poll
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
 	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	output = ', '.join([p.question for p in latest_poll_list]);
-	return HttpResponse(output)
+	context = {'latest_poll_list' : latest_poll_list,}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+	try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	return render(request, 'polls/detail.html', {'poll':poll})
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
