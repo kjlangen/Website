37f59c18ead8233c21c3cb7c9e3b59024dcd7002
kjlangen <fenwickhawks@gmail.com>
2014-08-13 21:00:31 -0400

    Learned more of the django templating language + how to write forms without getting csrf complaints.

--- DynamicWebsite/polls/views.py
+++ DynamicWebsite/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from polls.models import Poll
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 from django.shortcuts import render
 
 # Create your views here.
@@ -20,4 +21,12 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting at poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html', { 'poll' : p, 'error_message' : "You didn't select a choice."})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
